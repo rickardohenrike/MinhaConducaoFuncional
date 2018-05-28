61b5de2001302b16fac8d598496ef9ba89c1be3c
Ricardo <ricardohenrique_sg@hotmail.com>
2018-05-28 00:35:02 -0300

    Atualização do index e remoção dos logs de minhasrotas

--- js/rotas.js
+++ js/rotas.js
@@ -6,24 +6,21 @@ function minhasrotas() {
     var html = "";
 
     estudante.forEach(function(retorno){
-      console.log(retorno.val().cod_estudante);
+    
       firebase.database().ref('estudante/'+retorno.val().cod_estudante)
 	    .on("value", function(estudante){
 
-        console.log(estudante.val().endereco);
-        $('#teste').text(estudante.val().endereco);
-
-        html = '<tr><td>'+estudante.val().endereço+'</td></tr>';
+    
 
 
-        // html += '<tr>'+             
-			  // '<td>'+estudante.val().nomeest+'</td>'+
-			  // '<td>'+estudante.val().endereco+'</td>'+
-        // '<td>'+estudante.val().escola+'</td>'+
-        // '<td><a href="listarresponsavel.html" class="btn btn-default">Ir <span class="glyphicon glyphicon-search"></span></a></td>'+
-		  	// '<td> <input type="checkbox" class="form-check-input" id="exampleCheck1"> <label class="form-check-label" for="exampleCheck1">Faltou</label></td>'+
+         html += '<tr>'+             
+			   '<td>'+estudante.val().nomeest+'</td>'+
+			   '<td>'+estudante.val().endereco+'</td>'+
+         '<td>'+estudante.val().escola+'</td>'+
+         '<td><a href="listarresponsavel.html" class="btn btn-default">Ir <span class="glyphicon glyphicon-search"></span></a></td>'+
+		  	 '<td> <input type="checkbox" class="form-check-input" id="exampleCheck1"> <label class="form-check-label" for="exampleCheck1">Faltou</label></td>'+
 			 
-        //     '</tr>';
+             '</tr>';
           });
         
           
