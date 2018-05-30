95fc96559194ea0df69de1c0305ef799dee80d36
Enzo Teixeira <enzo_jesus@icloud.com>
2018-05-30 17:43:47 -0300

    não concluído

---
+++ js/geo.js
@@ -0,0 +1,13 @@
+//geolocationPage
+var x = document.getElementById("geoLocation");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
